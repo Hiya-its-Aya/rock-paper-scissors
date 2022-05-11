8b042059015534f2a921cb42ea4c482211397109
ayamoosa123 <ayamoosa98@gmail.com>
2022-05-11 00:30:25 -0700

    Add game function

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 //make computer play rps at random
 
 function computerPlay(){
@@ -11,44 +7,71 @@ function computerPlay(){
     return(compPlay);
 }
 
+
+
+
 function playRound(playerSelection, computerSelection) {
-    var playLow = playerSelection.toLowerCase();
+    var playLow = playerSelection.toLowerCase()
 
     // a shit ton of if, else for possible combo
     if(playLow == "rock" && computerSelection == "paper"){
-        console.log("Computer Wins: Paper beats rock.")
+        console.log("Computer Wins: Paper beats rock.");
+        return "compWin";
     }
 
     else if(playLow == "rock" && computerSelection == "scissors"){
-        console.log("You Win: Rock beats scissors!")
+        console.log("You Win: Rock beats scissors!");
+        return "userWin";
     }
 
     else if(playLow == "scissors" && computerSelection == "rock"){
-        console.log("Computer Wins: Rock beats scissors.")
+        console.log("Computer Wins: Rock beats scissors.");
+        return "userWin";
     }
 
     else if(playLow == "scissors" && computerSelection == "paper"){
-        console.log("You Win: Scissors beats paper!")
+        console.log("You Win: Scissors beats paper!");
+        return "userWin";
     }
 
     else if(playLow == "paper" && computerSelection == "rock"){
-        console.log("You Win: Paper beats rock!")
+        console.log("You Win: Paper beats rock!");
+        return "userWin";
     }
 
     else if(playLow == "paper" && computerSelection == "scissors"){
         console.log("Computer Wins: Scissors beats paper.")
+        return "compWin";
     }
     else{
-        console.log("Tie.")
+        console.log("Tie.");
+        return "tie";
     }
-    
 }
 
-//Make it go to 5 rounds
-function game(){
-    function playRound(){
 
+//Make it go to 5 rounds
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        let outcome =  playRound(playerSelection, computerSelection);
+        let numUserWin = 0;
+        let numCompWin = 0;
+        if(outcome === "compWin"){
+            numCompWin++;
+            console.log(numCompWin);
+            i++;
+        }
+        else if(outcome === "userWin"){
+            numUserWin++;
+            console.log(numUserWin);
+            i++
+        }
+        else{
+            return "tie";
+        }
     }
+    
 }
 
 
@@ -63,4 +86,5 @@ let play = [
 const playerSelection = prompt("Enter rock, paper, or scissors: " )
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+game()
 
